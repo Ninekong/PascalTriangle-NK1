bd55d75cdd0dca034990120991f9c6a5558a3435
Ninekong <122553014+Ninekong@users.noreply.github.com>
2023-05-15 19:05:12 +0700

    Add files via upload

---
+++ PascalTriangle NK1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of rows: ");
+            int numRows = int.Parse(Console.ReadLine());
+
+            if (numRows < 0)
+            {
+                Console.WriteLine("Invalid Pascal's triangle row number.");
+            }
+            else
+            {
+                for (int i = 0; i <= numRows; i++)
+                {
+                    int value = 1;
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write(value + " ");
+                        value = value * (i - j) / (j + 1);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
